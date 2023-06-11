4894fcdc248371a91aec0f8c623be825b8cef349
Albar00n <mgrushi73@gmail.com>
2023-06-11 17:05:49 +0400

    add stripe

--- src/api/order/controllers/order.ts
+++ src/api/order/controllers/order.ts
@@ -19,24 +19,13 @@ module.exports = createCoreController('api::order.order', ({ strapi }) => ({
             product_data: {
               name: product.title,
             },
-            unit_amount: product.price * 100,
+            // unit_amount: product.price * 100,
+            unit_amount: Math.round(product.price * 100),
           },
-          quantity: product.amount,
+          quantity: product.quantity,
         }
       })
-      const sampleProduct = products.find((product) => product.package)
-      if (sampleProduct) {
-        lineItems.push({
-          price_data: {
-            currency: 'aed',
-            product_data: {
-              name: sampleProduct.product.title,
-            },
-            unit_amount: sampleProduct.price * 100,
-          },
-          quantity: 1,
-        })
-      }
+     
 
       const session = await stripe.checkout.sessions.create({
         // shipping_address_collection: { allowed_countries: ["US", "UAE"] },
@@ -45,28 +34,11 @@ module.exports = createCoreController('api::order.order', ({ strapi }) => ({
         success_url: process.env.CLIENT_URL + '/success',
         cancel_url: process.env.CLIENT_URL + '?success=false',
         line_items: lineItems,
-        // [
-        //   {
-        //     price_data: {
-        //       currency: "aed",
-        //       product_data: {
-        //         name: "Hello Kitty",
-        //       },
-        //       unit_amount: 1000,
-        //     },
-        //     quantity: 1,
-        //   },
-        // ],
       })
 
-      await strapi.service('api::order.order').create({
-        data: {
-          products,
-          stripeId: session.id,
-          // size: session.selectedSize,
-          // img: session.img,
-        },
-      })
+      await strapi
+        .service('api::order.order')
+        .create({ data: { products, stripeId: session.id } })
 
       return { stripeSession: session }
     } catch (error) {
@@ -75,3 +47,5 @@ module.exports = createCoreController('api::order.order', ({ strapi }) => ({
     }
   },
 }))
+
+// quantity: product.quantity,
